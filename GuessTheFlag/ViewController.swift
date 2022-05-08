c355e49145ab0803d79e53929575d89de4faa05b
Bucerella <buseerkus@gmail.com>
2022-05-08 19:53:12 +0300

    Initial Commit

---
+++ GuessTheFlag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuessTheFlag
+//
+//  Created by Bucerella on 8.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
